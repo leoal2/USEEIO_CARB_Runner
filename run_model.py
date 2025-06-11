28bf2e66e9f4678b957ee28002b1c532f208dedb
Leo Guillotte <Leo.Guillotte@arb.ca.gov>
2025-06-11 16:57:38 -0700

    Update run_model.py

--- run_model.py
+++ run_model.py
@@ -30,7 +30,9 @@ import numpy as np
 import rpy2.robjects as ro
 from rpy2.robjects.packages import importr
 
-ro.r(f'Sys.setenv(STATEIOR_DATADIR = "{stateio_dir.replace(os.sep, '/')}")')
+#ro.r(f'Sys.setenv(STATEIOR_DATADIR = "{stateio_dir.replace(os.sep, '/')}")')
+ro.r(f"Sys.setenv(STATEIOR_DATADIR = '{stateio_dir.replace(os.sep, '/')}')")
+
 
 # Load USEEIO in R
 useeior = importr("useeior")
